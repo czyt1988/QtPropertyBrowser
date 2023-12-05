6421cb2bd8f8ea474422596c6e8d435934ed489f
czyt1988 <czy.t@163.com>
2023-12-05 09:07:25 +0800

    模板类去除dll导出

--- src/qtpropertybrowser.h
+++ src/qtpropertybrowser.h
@@ -152,7 +152,7 @@ protected Q_SLOTS:
 };
 
 template< class PropertyManager >
-class QT_QTPROPERTYBROWSER_EXPORT QtAbstractEditorFactory : public QtAbstractEditorFactoryBase
+class QtAbstractEditorFactory : public QtAbstractEditorFactoryBase
 {
 public:
     explicit QtAbstractEditorFactory(QObject* parent) : QtAbstractEditorFactoryBase(parent)
